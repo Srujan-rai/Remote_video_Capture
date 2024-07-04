a005490882e7a5e277a5821dd280d2cd7b2b5071
Srujan-rai <raisrujan@gmail.com>
2024-07-04 21:18:18 +0530

    threading to capture object

--- app.py
+++ app.py
@@ -1,55 +1,56 @@
 from flask import Flask, render_template, Response
 import cv2
+import socket
+import requests
+import threading
 
 app = Flask(__name__)
 
+
 cap = None
+frame = None
 
 def initialize_camera(index):
-    """Initialize camera capture."""
     global cap
     cap = cv2.VideoCapture(index)
     if not cap.isOpened():
         print("Error: Could not open camera with index", index)
+        cap = None
         return False
     return True
 
-def release_camera():
-    """Release camera capture."""
-    global cap
-    if cap is not None:
-        cap.release()
-        cap = None
-
-@app.route('/')
-def index():
-    """Render the main page."""
-    return render_template('index.html')
-
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    camera_index = 0
-    if not initialize_camera(camera_index):
-        return "<h1>Error: Could not open camera.</h1>"
-    return Response(process_video(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-def process_video():
-    """Process video frames and stream."""
-    global cap
-    while True:
+def capture_frames():
+    global cap, frame
+    while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             print("Error: Could not read frame.")
             break
+    cap.release()
 
+@app.route('/')
+def index():
+
+    return render_template('index.html', local_ip=0, public_ip=0)
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(stream_video(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def stream_video():
+    global frame
+    while True:
+        if frame is None:
+            continue
         ret, jpeg = cv2.imencode('.jpg', frame)
         if jpeg is not None:
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
 
 if __name__ == "__main__":
-    try:
+    if initialize_camera(0):
+        capture_thread = threading.Thread(target=capture_frames)
+        capture_thread.start()
         app.run(host='0.0.0.0', port=5000, debug=False)
-    finally:
-        release_camera()
+    else:
+        print("Error: Could not initialize camera.")
---
+++ gunicorn_config.py
@@ -0,0 +1,4 @@
+bind='0.0.0.0:5000'
+workers=8
+threads=2
+timeout=120
