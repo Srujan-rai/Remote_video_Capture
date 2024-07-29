b43bd65f411264e76f7378643b715eb1d0cca5ff
Srujan-rai <raisrujan@gmail.com>
2024-07-29 21:07:01 +0530

    codes added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Remote_video_Capture
+python based application that can be used to rematly access the camera and monitor
--- app.py
+++ app.py
@@ -53,4 +53,4 @@ if __name__ == "__main__":
         capture_thread.start()
         app.run(host='0.0.0.0', port=5000, debug=False)
     else:
-        print("Error: Could not initialize camera.")
+        print("Error: Could not initialize camera and live telecast.")
